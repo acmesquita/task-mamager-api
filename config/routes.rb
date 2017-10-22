7d749f0161f0df6c65f34026e5b56e25b16c5b01
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-10-22 16:17:22 -0300

    Config the routes file for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Configurando a rota para ser acessada assim: api.site.com/controller
+  # subdomain -> adicionar 'api' no inicio do site
+  # path -> '/' defini que deve ser inforamado o nome do controller logo após o '.com' 
+  namespace :api, defaults: { format: :json }, contraints: { subdomain: 'api'}, path: '/'  do
+    
+
+  end
 end
