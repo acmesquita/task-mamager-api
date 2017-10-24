108e681aa0084eab2f83cef02f0dec685e1b0d2f
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-10-23 22:53:32 -0300

    adding devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 load './lib/tasks/api_version_constraint.rb'
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #Configurando a rota para ser acessada assim: api.site.com/controller
