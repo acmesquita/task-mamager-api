ac8bde206b56fa581daa09b7040693c87f123f30
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-10-23 23:37:21 -0300

    examples for testing the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  before {@user = FactoryBot.build(:user)}
+  # subject { build(:user) }
+  # before {@user = FactoryBot.build(:user)}
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid  }
+  # it { expect(@user).to respond_to(:name) }
+  # it { expect(@user).to respond_to(:password) }
+  # it { expect(@user).to respond_to(:password_confirmation) }
+  # it { expect(@user).to be_valid  }
   
+  # it { expect(subject).to be_valid  }
+  
+  # Três formas de fazer o mesmo teste
+  # it { expect(@user).to respond_to(:email) }
+  # it { expect(subject).to respond_to(:email) }
+  # it { is_expected.to respond_to(:email) }
 end
